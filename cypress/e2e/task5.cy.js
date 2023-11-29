3b932c0625f46c951b7707490ab70d6d0e9af4db
Raghad Abu Baker <raghad.abubaker@endeavorpal.com>
2023-11-29 23:28:32 +0200

    Solution for task5

--- cypress/e2e/task5.cy.js
+++ cypress/e2e/task5.cy.js
@@ -11,15 +11,29 @@ describe("Check create an account page", () => {
     cy.get(".submit").should("be.visible");
   });
 
+  //Happy scenario
   it("verify from create account - Happy scenario", () => {
     cy.visit("/customer/account/create/");
-    cy.get("#firstname").type("Jermaine11141");
-    cy.get("#lastname").type("Johnson1111");
-    cy.get("#email_address").type("lusep111111@mailinator.com");
+    cy.get("#firstname").type("Jermainqqqpde");
+    cy.get("#lastname").type("Johnsontrrrrr");
+    cy.get("#email_address").type("lusep8dskjjmnd@mailinator.com");
     cy.get("#password").type("raghad9797#@");
     cy.get("#password-confirmation").type("raghad9797#@");
     cy.get(".submit").click();
     cy.wait(2000);
     cy.get(".messages [data-bind]").last().should("have.text","Thank you for registering with Main Website Store.");
+  });
+
+  //Bad scenario
+  it("verify from create account with an already existing account - Bad scenario", () => {
+    cy.visit("/customer/account/create/");
+    cy.get("#firstname").type("Jermaine");
+    cy.get("#lastname").type("Johnson");
+    cy.get("#email_address").type("lusep@mailinator.com");
+    cy.get("#password").type("raghad9797#@");
+    cy.get("#password-confirmation").type("raghad9797#@");
+    cy.get(".submit").click();
+    cy.wait(2000);
+    cy.get(".messages [data-bind]").last().should("contain","There is already an account with this email address. If you are sure that it is your email address, ");
   });
 });
