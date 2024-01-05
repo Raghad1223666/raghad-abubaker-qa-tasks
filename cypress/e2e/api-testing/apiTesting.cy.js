61b9d11d515ed41efd157bd853a4603ef4c18b1c
Raghad Abu Baker <raghad.abubaker@endeavorpal.com>
2024-01-05 12:52:54 +0200

    Api Testing

---
+++ cypress/e2e/api-testing/apiTesting.cy.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+describe("Api-Testing", () => {
+  it("Simple Request", () => {
+    cy.request("GET", "https://www.bstackdemo.com/").then((response) => {
+      expect(response.status).to.be.eq(200);
+    });
+  });
+
+  it("GET Request", () => {
+    cy.request({
+      url: "https://reqres.in/api/users",
+    }).then((response) => {
+      expect(response.status).to.be.eq(200);
+      expect(response.statusText).to.be.equal("OK");
+    });
+  });
+
+  it("POST Request", () => {
+    cy.request({
+      method: "POST",
+      url: "https://reqres.in/api/users",
+      body: { name: "Raghad Abu Baker", job: "Software Engineer" },
+    }).then((response) => {
+      expect(response.status).to.be.equal(201);
+      expect(response.statusText).to.be.equal("Created");
+      expect(response.body.name).to.be.equal("Raghad Abu Baker");
+    });
+  });
+
+  it("PUT Request", () => {
+    cy.request({
+      method: "PUT",
+      url: "https://reqres.in/api/users/2",
+      body: { first_name: "Rayan", last_name: "Abu Baker" },
+    }).then((response) => {
+      expect(response.status).to.be.eq(200);
+      expect(response.body.first_name).to.be.equal("Rayan");
+      expect(response.body.last_name).to.be.equal("Abu Baker");
+    });
+  });
+
+  it("PATCH Request", () => {
+    cy.request({
+      method: "PATCH",
+      url: "https://reqres.in/api/users/1",
+      body: { first_name: "Rayan", last_name: "Abu Baker" },
+    }).then((response) => {
+      expect(response.status).to.be.eq(200);
+      expect(response.body.first_name).to.be.equal("Rayan");
+      expect(response.body.last_name).to.be.equal("Abu Baker");
+    });
+  });
+
+  it("DELETE Request", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://reqres.in/api/users/1",
+    }).then((response) => {
+      expect(response.status).to.be.eq(204);
+    });
+  });
+
+  it('GET Request - with query', () => {
+    cy.request({method: 'GET', url: 'http://api.weatherstack.com/current', qs: {
+    access_key: Cypress.env('access_key'),
+    query: "Palestine"
+    }}).then((response) =>{
+    expect(response.status).to.eq(200);
+    })
+    })
+});
